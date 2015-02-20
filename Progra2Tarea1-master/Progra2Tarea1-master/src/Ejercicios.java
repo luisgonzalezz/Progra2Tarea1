420dc6b07c851dc34dd1f64a0b1fa6949104f31a
luisgonzalezz <luis.rigoma@gmail.com>
2015-02-20 15:31:17 -0600

    Update Ejercicios.java

--- Progra2Tarea1-master/Progra2Tarea1-master/src/Ejercicios.java
+++ Progra2Tarea1-master/Progra2Tarea1-master/src/Ejercicios.java
@@ -52,7 +52,7 @@ public class Ejercicios
 	//devuelve el atributo "x" de "MiClase"
 	static int getX(MiClase mi_clase)
 	{
-		return mi_clase.x;  //Ya saque 100!!!! Devuelve el atributo "x" de miclase
+		return mi_clase.x;  //Ya saque 100!!!! Devuelve el atributo "x" de miClase
 	}
 	
 	//implementar la funcion "getXPor2()" descrita en la clase "MiClase"
@@ -66,30 +66,34 @@ public class Ejercicios
 	//No modificar esta funcion
 	static int implementarGetXMasY(MiClase mi_clase, int y)
 	{
-		return mi_clase.getXMasY(y); //
+		return mi_clase.getXMasY(y); //Llama la funcion getXMasY de la variable mi_clase
 	}
 	
 	//devuelve la funcion fibonacci de "num"
 	//nota: puedes consultar la funcion en http://es.wikipedia.org/wiki/Sucesi%C3%B3n_de_Fibonacci
 	static int fibonacci(int num)
 	{
-		if(num==1) 
-		return 1;
-		else if (num==0) 
-		return 0;
-		return fibonacci(num-1) + fibonacci(num-2);
-		}
+		if(num==1) return 1; // num es 1 regresa 1
+else if (num==0) return 0; // si num es 0 regresa 0
+return fibonacci(num-1) + fibonacci(num-2); // si no retorna la suma de la funcion fibonacci del numero -1,
+                                            // mas la funcion de fibonacci del numero -2
+		}                           
 
 	//realizar la siguiente funcion recursiva:
 	//funcionRecursiva(n) = funcionRecursiva(n-1)*2+1
 	//donde: funcionRecursiva(0) = 1 y funcionRecursiva(1) = 2
 	static int funcionRecursiva(int num)
 	{
-		if(num==0) 
-	    return 1;
-		else if(num==1) 
-		return 2;
-		return funcionRecursiva(num-1)*2+1;
+	if(num==0) return 1; // si num es 0 devuelve 1
+else if(num==1) return 2; // si num es 1 devuelve 2
+return funcionRecursiva(num-1)*2+1; // y para cualquier otro numero num llama a la misma funcion 
+                                    //donde numero es menos uno y multiplicado por 2 y luego se le suma 1
+}
+public static void main(String[] args)
+{
+}
+}
+
 	}
 	
 	public static void main(String[] args)
