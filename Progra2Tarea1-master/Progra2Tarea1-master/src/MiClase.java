fbf6ef43419e7630fe2867cbfff724b29c5fe376
luisgonzalezz <luis.rigoma@gmail.com>
2015-02-20 15:32:54 -0600

    Update MiClase.java

--- Progra2Tarea1-master/Progra2Tarea1-master/src/MiClase.java
+++ Progra2Tarea1-master/Progra2Tarea1-master/src/MiClase.java
@@ -3,18 +3,18 @@ public class MiClase {
 	int x;
 	MiClase(int x)
 	{
-		this.x = x;
+		this.x = x;  //De aqui obtiene la variable x
 	}
 	
 	//devuelve "x" multiplicado por dos
 	int getXPor2()
 	{
-		return (x*2);
+		return (x*2); //Devuelve x multiplicado por 2
 	}
 	
 	//devuelve la suma de "x" y "y"
 	int getXMasY(int y)
 	{
-		return (x+y);
+		return (x+y);  //suma variables x + y
 	}
 }
